3833baf62daa9a6a430a83dfa5e0e4694d14ee47
Leslie Tang <49802619+tangweejieleslie@users.noreply.github.com>
2020-12-25 20:41:30 +0800

    updated to read image

--- image processing/read_img.py
+++ image processing/read_img.py
@@ -1,17 +1,32 @@
 import cv2
 import pytesseract
+import os
+
+# References
+# https://stackoverflow.com/questions/42683517/loop-through-folder-in-python-and-open-files-throws-an-error
+# https://www.youtube.com/watch?v=6DjFscX4I_c&ab_channel=Murtaza%27sWorkshop-RoboticsandAI
+
+imageFolder = "C:\\Users\\65909\\Documents\\Github\\PythonSandbox\\image processing\\trial-images\\guardian_tales_data"
 
 pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
-img = cv2.imread("words.PNG")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+def get_text(img):
+    print(pytesseract.image_to_string(img))
+
+    cv2.imshow('Result', img)
+    cv2.waitKey(0)
 
 
 
-print(pytesseract.image_to_string(img))
+for filename in os.listdir(imageFolder):
+    if filename.endswith('.PNG'):
+        image_path = os.path.join(imageFolder, filename)
+        print(image_path)
+        img = cv2.imread(image_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        get_text(img)
 
 
-cv2.imshow('Result', img)
-cv2.waitKey(0)
 
 
