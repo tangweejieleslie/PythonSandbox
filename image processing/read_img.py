4becccc80e55dcf7f33ef46048cf4b390789d24c
Leslie Tang <49802619+tangweejieleslie@users.noreply.github.com>
2020-12-25 19:51:35 +0800

    init pytesseract for image to string function

---
+++ image processing/read_img.py
@@ -0,0 +1,17 @@
+import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+
+img = cv2.imread("words.PNG")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+
+print(pytesseract.image_to_string(img))
+
+
+cv2.imshow('Result', img)
+cv2.waitKey(0)
+
+
